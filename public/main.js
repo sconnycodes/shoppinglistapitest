b8e5ec3a2d923fd866e1c8ac8c491c9ae14f1844
Mark Mac <markmac.web@gmail.com>
2022-06-24 22:48:27 +0100

    added edit and delete buttons to ejs template

---
+++ public/main.js
@@ -0,0 +1,12 @@
+const edit = document.querySelector('#edit')
+
+edit.addEventListener('click', _ => {
+  // Send PUT Request here
+  
+})
+
+const deleteItem = document.querySelector('#delete')
+
+deleteItem.addEventListener('click', _ => {
+  // Send PUT Request here
+})
