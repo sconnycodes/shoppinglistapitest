d2c5f6f591996cb79179782d8a4b04343f2e73a2
Mark Mac <markmac.web@gmail.com>
2022-06-24 17:49:54 +0100

    added ejs template, data from mongodb now displays in client

--- server.js
+++ server.js
@@ -7,13 +7,20 @@ const {MongoClient, ObjectId } = require('mongodb')
 require('dotenv').config()
 const PORT = 8000
 
-//stuff to be used:
-app.use(express.json())
+//stuff to be used, middleware etc:
+// app.use(express.json())
 app.use(express.static("public"))
 app.use(express.urlencoded({extended : true}))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(cors())
 
+app.set('view engine', 'ejs')
+
+// Gotta listen out for stuff: 
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
 
 // Database connection:
 let db,
@@ -26,19 +33,26 @@ MongoClient.connect(dbConnectionStr)
         console.log(`Connected to database`)
         db = client.db(dbName)
         collection = db.collection('shoppinglist')
-        app.set('view engine', 'ejs')
+        
     })
     .catch(error => {console.error(error)});
 
 
 // The CRUD parts:
+// get
 app.get(`/`, (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    // res.sendFile(__dirname + "/index.html")
+    collection.find().toArray()
+        .then(results => {
+            console.log(results)
+            res.render("index.ejs", { shopping : results })
+        })
+        .catch(error => console.error(error))
+        
+    
 });
 
-// app.get(`/api/shoppinglist`, (req, res) => {
-//     res.json(shoppingList)
-// });
+
 
 // Adding items to the list:
 app.post('/shoppinglist', (req, res) => {
@@ -46,13 +60,12 @@ app.post('/shoppinglist', (req, res) => {
     collection.insertOne(req.body)
     .then(result => {
         console.log(result)
+        res.redirect("/")
     })
     .catch(error => console.error(error))
 });
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-});
+
 
 
 // const shoppingList = [
